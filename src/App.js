f7a43bacae7404830b9d84dcb490bd3c335ea37b
APIgames2 <111185287+APIgames2@users.noreply.github.com>
2023-05-03 13:49:56 +0200

    supersite

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <div className=''>
-      <ReactPlayer
-        width={"100%"}
-        height={"100%"}
-        controls
-        url={"./blue.mp4"}
-      />
-      </div>
       <div className="p-4 md:flex md:flex-row md:justify-center">
         <div className="md:p-4">
           <CardHome/>
